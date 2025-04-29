7e8f3855c55f1e30ef884526723588a18fd89b6f
antonia_tjto <antonia.sousa@tjto.jus.br>
2025-04-29 17:55:22 -0300

    first commit

---
+++ produtosapispring/src/main/java/io/github/cursodsousa/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,30 @@
+package io.github.cursodsousa.produtosapi.controller;
+
+import io.github.cursodsousa.produtosapi.model.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.github.cursodsousa.produtosapi.repository.ProdutoRepository;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public Produto salvar (@RequestBody Produto produto){
+        System.out.println("Produto recebido" + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
+        return produto;
+    }
+}
